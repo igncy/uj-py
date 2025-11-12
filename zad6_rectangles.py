0e59dedd1ced1e3a365535b2a2434d89dfeeb761
ignacy <ignacysiklucki@gmail.com>
2025-11-12 17:24:24 +0100

    6

--- zad6_rectangles.py
+++ zad6_rectangles.py
@@ -13,17 +13,26 @@ class Rectangle:
     def __repr__(self):         # "Rectangle(x1, y1, x2, y2)"
         return f'Rectangle({self.pt1.x}, {self.pt1.y}, {self.pt2.x}, {self.pt2.y})'
 
+    def __norm(self):
+        return Rectangle(min(self.pt1.x, self.pt2.x),
+                         min(self.pt1.y, self.pt2.y),
+                         max(self.pt1.x, self.pt2.x),
+                         max(self.pt1.y, self.pt2.y))
+
     def __eq__(self, other):    # obsługa rect1 == rect2
-        return
+        r1=self.__norm()
+        r2=other.__norm()
+        return r1.pt1 == r2.pt1 and r1.pt2 == r2.pt2
 
     def __ne__(self, other):        # obsługa rect1 != rect2
         return not self == other
 
     def center(self):           # zwraca środek prostokąta
-        return
+        r = self.__norm()
+        return Point(r.pt1.x+(r.pt2.x-r.pt1.x)/2, r.pt1.y+(r.pt2.y-r.pt1.y)/2)
 
     def area(self):             # pole powierzchni
-        return
+        return abs(self.pt1.x-self.pt2.x)*abs(self.pt1.y-self.pt2.y)
 
     def move(self, x, y):       # przesunięcie o (x, y)
         p=Point(x,y)
@@ -52,17 +61,20 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(self.a==Rectangle(0,2,5,10), True)
 
     def test_ne(self):
-        self.assertEqual(self.a==self.b, True)
-        self.assertEqual(self.a==Rectangle(0,2,5,10), False)
+        self.assertEqual(self.a!=self.b, True)
+        self.assertEqual(self.a!=Rectangle(0,2,5,10), False)
 
     def test_center(self):
-        self.assertEqual()
+        self.assertEqual(self.a.center(),Point(2.5,6))
+        self.assertEqual(self.b.center(),Point(2,4.5))
 
     def test_area(self):
-        self.assertEqual()
+        self.assertEqual(self.a.area(),40)
+        self.assertEqual(self.b.area(),6)
 
     def test_move(self):
-        self.assertEqual()
+        self.a.move(1,1)
+        self.assertEqual(self.a, Rectangle(1,3,6,11))
 
     def tearDown(self): pass
 
