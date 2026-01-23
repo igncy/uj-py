a32e49ce8af0555af07a966cbcbecfc31eb18a54
ignacy <ignacysiklucki@gmail.com>
2026-01-23 23:06:15 +0100

    mandelbrot

--- mandelbrot/mandelbrot.py
+++ mandelbrot/mandelbrot.py
@@ -5,9 +5,11 @@ import os
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = ''; import pygame as pg
 
+ROWS_PER_FRAME: int = 20
+
 #TODO pdoc
 
-def colour(n):
+def colour(n) -> tuple[int, int, int]:
     if n == 0: return 0, 0, 0
     return tuple(map(lambda x: int(255 * x), colorsys.hsv_to_rgb(0.95 * n, 0.8, 1.0)))
 
@@ -28,10 +30,10 @@ class App:
         pg.display.set_caption('Mandelbrot set')
         self.bg_colour = 'black'
         self.screen = pg.display.set_mode((self.width, self.height), pg.RESIZABLE)
-        self.temp_surface = pg.Surface((self.width, self.height), pg.RESIZABLE)
-        self.pixels = None
+        self.buffer = pg.Surface((self.width, self.height))
         self.running = False
         self.to_draw = True
+        self.drawn_rows = 0
         self.colours = [pg.Color(colour(i/max_iter)) for i in range(self.max_iter+1)]
         x_length = 3.0
         y_length = 3.0
@@ -49,102 +51,179 @@ class App:
 
     def update_size(self):
         self.width, self.height = pg.display.get_surface().get_size()
+        self.buffer = pg.Surface((self.width, self.height))
+        self.drawn_rows = 0
+        self.to_draw = True
 
     def _f(self, z, c):
         return z*z + c if self.power == 2 else z**self.power + c
 
-    def mandelbrot(self, width, height):
-        pixels = pg.PixelArray(self.temp_surface)
+    def mandelbrot(self):
+        pixels = pg.PixelArray(self.buffer)
+        width, height = self.width, self.height
+        x_min = self.x_min
+        x_max = self.x_max
+        y_min = self.y_min
+        y_max = self.y_max
+        max_iter = self.max_iter
+        bound2 = self.bound * self.bound
+        colours = self.colours
 
-        for y in range(height):
-            pg.event.pump()
-            y_ = self.y_min + (self.y_max-self.y_min) * y / (height-1)
-            for x in range(width):
-                x_ = self.x_min + (self.x_max-self.x_min) * x / (width-1)
-                z = 0
-                c = complex(x_, y_)
-                for i in range(self.max_iter):
-                    # if abs(z) >= self.bound:
-                    if z.real*z.real + z.imag*z.imag >= self.bound*self.bound:
-                        iters = i
-                        break
+        if self.power == 2:
+            for y in range(ROWS_PER_FRAME):
+                if self.drawn_rows >= height:
+                    pixels.close()
+                    return True
+                y = self.drawn_rows
+                y_ = y_min + (y_max-y_min) * y / (height-1)
+                for x in range(width):
+                    x_ = x_min + (x_max-x_min) * x / (width-1)
+                    # z = 0 + 0j
+                    # c = complex(x_, y_)
+                    # c = x_ + y_*1j
+                    zr = zi = 0.0
+                    for i in range(max_iter):
+                        zr2 = zr * zr
+                        zi2 = zi * zi
+                        # if abs(z) >= self.bound:
+                        # if z.real*z.real + z.imag*z.imag >= self.bound*self.bound:
+                        if zr2 + zi2 >= bound2:
+                            iters = i
+                            break
+                        else:
+                            # z = self._f(z, c)
+                            zi = 2*zr*zi + y_
+                            zr = zr2 - zi2 + x_
                     else:
-                        z = self._f(z, c)
-                else:
-                    iters = 0
-                pixels[x, y] = self.colours[iters]
+                        iters = 0
+                    pixels[x, y] = colours[iters]
+                self.drawn_rows += 1
+        else:
+            for y in range(ROWS_PER_FRAME):
+                if self.drawn_rows >= height:
+                    pixels.close()
+                    return True
+                y = self.drawn_rows
+                y_ = y_min + (y_max-y_min) * y / (height-1)
+                for x in range(width):
+                    x_ = x_min + (x_max-x_min) * x / (width-1)
+                    z = 0 + 0j
+                    # c = complex(x_, y_)
+                    c = x_ + y_*1j
+                    for i in range(max_iter):
+                        # if abs(z) >= self.bound:
+                        if z.real*z.real + z.imag*z.imag >= self.bound*self.bound:
+                            iters = i
+                            break
+                        else:
+                            z = z**self.power + c
+                    else:
+                        iters = 0
+                    pixels[x, y] = colours[iters]
+                self.drawn_rows += 1
 
         pixels.close()
+        return False
 
     def draw_screen(self):
-        self.mandelbrot(self.width, self.height)
-        self.screen.blit(self.temp_surface, (self.x_min, self.y_min))
+        drawn_all = self.mandelbrot()
+        self.screen.blit(self.buffer, (0, 0))
         pg.display.flip()
+        if drawn_all: self.to_draw = False
 
-    def handle_key(self, key) -> bool:
+    def handle_keys(self, key=None) -> bool:
         dx = (self.x_max-self.x_min) * self.camera_step
         dy = (self.y_max-self.y_min) * self.camera_step
         zx = (self.x_max-self.x_min) * self.zoom_step
         zy = (self.y_max-self.y_min) * self.zoom_step
 
-        match key:
-            case pg.K_ESCAPE:
-                """Quit on Escape."""
-                return False
-            case pg.K_w | pg.K_UP:
+        if key:
+            match key:
+                case pg.K_ESCAPE:
+                    """Quit on Escape."""
+                    return False
+                case pg.K_w | pg.K_UP:
+                    """Move camera up on W or Up."""
+                    self.y_min -= dy
+                    self.y_max -= dy
+                case pg.K_s | pg.K_DOWN:
+                    """Move camera down on S or Down."""
+                    self.y_min += dy
+                    self.y_max += dy
+                case pg.K_a | pg.K_LEFT:
+                    """Move camera left on A or Left."""
+                    self.x_min -= dx
+                    self.x_max -= dx
+                case pg.K_d | pg.K_RIGHT:
+                    """Move camera right on D or Right."""
+                    self.x_min += dx
+                    self.x_max += dx
+                case pg.K_e | pg.K_PAGEUP:
+                    """Zoom in on E or Page Up."""
+                    self.x_min += zx
+                    self.x_max -= zx
+                    self.y_min += zy
+                    self.y_max -= zy
+                case pg.K_q | pg.K_PAGEDOWN:
+                    """Zoom out on Q or Page Down."""
+                    self.x_min -= zx
+                    self.x_max += zx
+                    self.y_min -= zy
+                    self.y_max += zy
+                case _:
+                    return True
+        else:
+            keys = pg.key.get_pressed()
+            if keys[pg.K_w] or keys[pg.K_UP]:
                 """Move camera up on W or Up."""
                 self.y_min -= dy
                 self.y_max -= dy
-            case pg.K_s | pg.K_DOWN:
+            if keys[pg.K_s] or keys[pg.K_DOWN]:
                 """Move camera down on S or Down."""
                 self.y_min += dy
                 self.y_max += dy
-            case pg.K_a | pg.K_LEFT:
+            if keys[pg.K_a] or keys[pg.K_LEFT]:
                 """Move camera left on A or Left."""
                 self.x_min -= dx
                 self.x_max -= dx
-            case pg.K_d | pg.K_RIGHT:
+            if keys[pg.K_d] or keys[pg.K_RIGHT]:
                 """Move camera right on D or Right."""
                 self.x_min += dx
                 self.x_max += dx
-            case pg.K_e | pg.K_PAGEUP:
+            if keys[pg.K_e] or keys[pg.K_PAGEUP]:
                 """Zoom in on E or Page Up."""
                 self.x_min += zx
                 self.x_max -= zx
                 self.y_min += zy
                 self.y_max -= zy
-            case pg.K_q | pg.K_PAGEDOWN:
+            if keys[pg.K_q] or keys[pg.K_PAGEDOWN]:
                 """Zoom out on Q or Page Down."""
                 self.x_min -= zx
                 self.x_max += zx
                 self.y_min -= zy
                 self.y_max += zy
-            case _:
-                return True
 
         self.to_draw = True
+        self.drawn_rows = 0
         return True
 
     def run(self):
         self.running = True
         while self.running:
-            self.update_size()
-
-            if self.to_draw:
-                self.draw_screen()
-                self.to_draw = False
+            if self.to_draw: self.draw_screen()
 
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     self.running = False
                     break
-                if event.type == pg.KEYDOWN:
-                    if not self.handle_key(event.key):
-                        self.running = False
-                        break
+                if event.type == pg.KEYDOWN and not self.handle_keys(event.key):
+                    self.running = False
+                    break
+                if any(pg.key.get_pressed()) and not self.handle_keys():
+                    self.running = False
+                    break
                 if event.type == pg.VIDEORESIZE:
                     self.update_size()
-                    self.to_draw = True
 
 
 if __name__ == '__main__':
